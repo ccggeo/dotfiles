5526799bfc04e5e6b47c5d5c23ecd986c97a691d
ccggeo <georgeconnorc@gmail.com>
2019-10-25 10:49:23 +0100

    ctags, and install nvim

--- vim/deployvim.sh
+++ vim/deployvim.sh
@@ -2,6 +2,9 @@
 bundle=$HOME/.vim/bundle
 colors=$HOME/.vim/colors
 backup=$HOME/.vim/backup
+nvim=$HOME/.config/nvim
+sudo apt install neovim exuberant-ctags -y
+
 if [ ! -d $bundle ]; then
   mkdir $bundle
   git clone https://github.com/VundleVim/Vundle.vim.git $bundle/Vundle.vim 
@@ -12,6 +15,11 @@ if [ ! -d $bundle ]; then
 fi
 if [ ! -d $backup ]; then
   mkdir $backup
+fi
+
+if [ ! -d $nvim ]; then
+  mkdir -p $nvim
+  cp init.vim $nvim
 fi
 
 cp vimrc ~/.vimrc
