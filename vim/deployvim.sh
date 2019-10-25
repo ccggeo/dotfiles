aab0f8c0c5a05ad783aff88bb1be63092e69d694
ccggeo <georgeconnorc@gmail.com>
2019-10-25 11:10:34 +0100

    added copy files

--- vim/deployvim.sh
+++ vim/deployvim.sh
@@ -8,19 +8,23 @@ sudo apt install neovim exuberant-ctags -y
 if [ ! -d $bundle ]; then
   mkdir $bundle
   git clone https://github.com/VundleVim/Vundle.vim.git $bundle/Vundle.vim 
-	if [ ! -d $colors ]; then
-	  mkdir $colors
-	  cp colors/* $colors 
-	fi
 fi
+
 if [ ! -d $backup ]; then
   mkdir $backup
 fi
 
+if [ ! -d $colors ]; then
+  mkdir $colors
+fi
+
+
 if [ ! -d $nvim ]; then
   mkdir -p $nvim
-  cp init.vim $nvim
 fi
 
 cp vimrc ~/.vimrc
+mv colors/* $colors 
+cp init.vim $nvim
+
 vim +PluginInstall +GoInstallBinaries +qall 
